d64d2d14ee4079f5f2cb82426c67e73d0d2c0dd4
Rodrigo Morcela <63730360+RodrigoMorcela@users.noreply.github.com>
2020-11-05 18:02:48 +0000

    Player Movement
    Player Movement scripts commit

---
+++ Sci-Fi_FPS_Demo/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    private NavMeshAgent _navAgent;
+
+    [SerializeField]
+    private float _speed;
+    private float _gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navAgent = GetComponent<NavMeshAgent>();
+
+        _speed = 3.5f;
+
+        _gravity = 9.81f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        Vector3 velocity = direction * _speed;
+
+        velocity.y -= _gravity;
+
+        velocity = transform.transform.TransformVector(velocity);
+
+        _navAgent.Move(velocity * Time.deltaTime);
+
+    }
+}
