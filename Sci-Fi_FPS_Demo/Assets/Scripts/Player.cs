0d228823d8b4a0a1c9411783757296cb986f6675
Rodrigo Morcela <rgm_99_@sapo.pt>
2020-11-08 13:38:09 +0000

    Weapon animtaions, raycasting, sound

--- Sci-Fi_FPS_Demo/Assets/Scripts/Player.cs
+++ Sci-Fi_FPS_Demo/Assets/Scripts/Player.cs
@@ -12,6 +12,17 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed;
     private float _gravity;
+    
+    [SerializeField]
+    private GameObject _muzzelFlash;
+
+    [SerializeField]
+    private GameObject _gameObjWeapon;
+
+    private Weapon _weapon;
+
+    [SerializeField]
+    private GameObject _hitMarkerPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +32,14 @@ public class Player : MonoBehaviour
         _speed = 3.5f;
 
         _gravity = 9.81f;
+
+        Cursor.visible = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _muzzelFlash.SetActive(false);
+
+        _weapon = _gameObjWeapon.GetComponent<Weapon>();
         
     }
 
@@ -28,6 +47,52 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+        Move();
+        MouseInput();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+    private void MouseInput()
+    {
+
+        if (Input.GetMouseButton(0))
+        {
+
+            _weapon.Fire();
+
+            _muzzelFlash.SetActive(true);
+
+            Ray originRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+            
+            if(Physics.Raycast(originRay, out hitInfo))
+            {
+                print("Hit: " + hitInfo);
+
+                GameObject newHitMarker = Instantiate(_hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+
+                Destroy(newHitMarker, 1f);
+
+            }
+ 
+        }
+        else
+        {
+            _muzzelFlash.SetActive(false);
+
+            _weapon.StopFire();
+        }
+
+    }
+
+    private void Move()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -40,6 +105,5 @@ public class Player : MonoBehaviour
         velocity = transform.transform.TransformVector(velocity);
 
         _navAgent.Move(velocity * Time.deltaTime);
-
     }
 }
